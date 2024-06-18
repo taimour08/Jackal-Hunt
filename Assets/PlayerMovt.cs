f85a939ed7fdeb3875991e4cf80848bcad0375e3
Taimour <mukhay@ttu.ee>
2024-06-18 12:05:48 +0300

    movement: control camera angles through mouse

--- Assets/PlayerMovt.cs
+++ Assets/PlayerMovt.cs
@@ -22,13 +22,22 @@ public class PlayerMovt : MonoBehaviour
     void Update()
     {
         Vector3 verticalMove = transform.forward * Input.GetAxis("Vertical"); // Input.GetAxis("Vertical") is a method in Unity that gets the value of the vertical input axis. Fwd/Bck.
-      //  Vector3 horizontalMove = transform.right * Input.GetAxis("Horizontal"); // 
+        Vector3 horizontalMove = transform.right * Input.GetAxis("Horizontal"); // 
 
-        moveInput = verticalMove;
-        moveInput.Normalize(); // so that diagnol movement is equal to the ver and hor movet, pythagoras. 1.41 ~ 1
+        moveInput = horizontalMove + verticalMove;
+       // moveInput.Normalize(); // so that diagnol movement is equal to the ver and hor movet, pythagoras. 1.41 ~ 1
         moveInput = moveInput * moveSpeed; 
-
+  
         CharacterController.Move(moveInput * Time.deltaTime); // Time.deltaTime, you ensure that the object moves at a consistent speed regardless of the frame rate. FR = images/sec
+        // The method takes a Vector3 parameter, which specifies the direction and distance to move the character.
+    
+        //camera rotation 
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+    
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        cameraTransform.rotation = Quaternion.Euler(cameraTransform.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+        
     
     }
 }
